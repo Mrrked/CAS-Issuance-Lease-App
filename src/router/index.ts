c72068a6e1ed0ee101535b88da844368c0bb4796
Mark Edison Rosario <rosariomark37@gmail.com>
2024-12-09 15:46:52 +0800

    remove logs

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,30 +43,25 @@ const hasValidTokens = (): boolean => {
   const refreshToken = localStorage.getItem('refresh') || '';
 
   if (!accessToken || !refreshToken) {
-    console.log('NO TOKEN')
+    // console.log('NO TOKEN')
     return false
   } else {
     const currentTime = Math.floor(Date.now() / 1000);
     const access_token_decoded = jwtDecode(accessToken) as ExtendedJWTPayload;
     const refresh_token_decoded = jwtDecode(refreshToken) as ExtendedJWTPayload;
 
-    console.log(new Date(), currentTime);
-    console.log(new Date(access_token_decoded.exp as number * 1000), access_token_decoded.exp);
-    console.log(new Date(refresh_token_decoded.exp as number * 1000), refresh_token_decoded.exp);
-    console.log(currentTime > (access_token_decoded.exp as number), currentTime > (refresh_token_decoded.exp as number));
-
     if (currentTime > (access_token_decoded.exp as number)) {
-      console.log('EXPIRED ACCESS TOKEN')
+      // console.log('EXPIRED ACCESS TOKEN')
 
       if (currentTime > (refresh_token_decoded.exp as number)) {
-        console.log('NO: EXPIRED REFRESH TOKEN')
+        // console.log('NO: EXPIRED REFRESH TOKEN')
         return false
       }
-      console.log('YES: EXPIRED ACCESS TOKEN BUT ACTIVE REFRESH TOKEN')
+      // console.log('YES: EXPIRED ACCESS TOKEN BUT ACTIVE REFRESH TOKEN')
       return true
     }
 
-    console.log('YES: ACTIVE ACCESS TOKEN')
+    // console.log('YES: ACTIVE ACCESS TOKEN')
     return true
   }
 

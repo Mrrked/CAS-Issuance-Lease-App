33fd8bf9e80a815e5c9c5ae876f103785456050b
Mark Edison Rosario <rosariomark37@gmail.com>
2024-12-09 15:44:29 +0800

    update router middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
 
+import { ExtendedJWTPayload } from '../store/types';
+import { jwtDecode } from 'jwt-decode';
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -35,17 +38,52 @@ const router = createRouter({
   routes
 })
 
+const hasValidTokens = (): boolean => {
+  const accessToken = localStorage.getItem('access') || '';
+  const refreshToken = localStorage.getItem('refresh') || '';
+
+  if (!accessToken || !refreshToken) {
+    console.log('NO TOKEN')
+    return false
+  } else {
+    const currentTime = Math.floor(Date.now() / 1000);
+    const access_token_decoded = jwtDecode(accessToken) as ExtendedJWTPayload;
+    const refresh_token_decoded = jwtDecode(refreshToken) as ExtendedJWTPayload;
+
+    console.log(new Date(), currentTime);
+    console.log(new Date(access_token_decoded.exp as number * 1000), access_token_decoded.exp);
+    console.log(new Date(refresh_token_decoded.exp as number * 1000), refresh_token_decoded.exp);
+    console.log(currentTime > (access_token_decoded.exp as number), currentTime > (refresh_token_decoded.exp as number));
+
+    if (currentTime > (access_token_decoded.exp as number)) {
+      console.log('EXPIRED ACCESS TOKEN')
+
+      if (currentTime > (refresh_token_decoded.exp as number)) {
+        console.log('NO: EXPIRED REFRESH TOKEN')
+        return false
+      }
+      console.log('YES: EXPIRED ACCESS TOKEN BUT ACTIVE REFRESH TOKEN')
+      return true
+    }
+
+    console.log('YES: ACTIVE ACCESS TOKEN')
+    return true
+  }
+
+}
+
 // Middleware
 router.beforeEach(async (to, _from, next,) => {
-  // console.log('FROM: ', from.fullPath);
-  // console.log('TO: ', to.fullPath);
 
-  const token = localStorage.getItem('access');
+  // HANDLE PROTECTED ROUTES
   if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (!token) {
+    if (!hasValidTokens()) {
       return next('/login');
     }
-  } else if (to.name === 'Login' && token) {
+  }
+
+  // REDIRECT TO LANDING PAGE IF USER IS AUTHENTICATED
+  else if (to.name === 'Login' && hasValidTokens()) {
     return next('/');
   }
 
